54f3fa448178b66d0e4ed251c55c213b799ef9c7
jmmccota <joao_marcos_cota@hotmail.com>
2015-03-19 20:47:03 -0300

    teste analise lexica

--- Kalameet/src/Compiler/AnalisadorLexico.java
+++ Kalameet/src/Compiler/AnalisadorLexico.java
@@ -1,19 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Compiler;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -22,7 +18,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
  * @author JM
  */
 public class AnalisadorLexico {
@@ -40,41 +35,41 @@ public class AnalisadorLexico {
 
     private void preencheLexemas() {
 //Aritmeticos
-        lexemas.put("+", "sum");
-        lexemas.put("-", "sub");
-        lexemas.put("*", "mult");
-        lexemas.put("x", "mult");
-        lexemas.put("/", "div");
-        lexemas.put(":", "div");
-        lexemas.put(".", ".");
-        lexemas.put(",", ".");
+        lexemas.put("+", "<sum>");
+        lexemas.put("-", "<sub>");
+        lexemas.put("*", "<mult>");
+        lexemas.put("x", "<mult>");
+        lexemas.put("/", "<div>");
+        lexemas.put(":", "<div>");
+        lexemas.put(".", "<.>");
+        lexemas.put(",", "<.>");
 //Comparativos
-        lexemas.put(">", "gt");
-        lexemas.put(">=", "gte");
-        lexemas.put("<", "lt");
-        lexemas.put("=<", "lte");
-        lexemas.put("==", "eq");
-        lexemas.put("!=", "eq");
+        lexemas.put(">", "<gt>");
+        lexemas.put(">=", "<gte>");
+        lexemas.put("<", "<lt>");
+        lexemas.put("=<", "<lte>");
+        lexemas.put("==", "<eq>");
+        lexemas.put("!=", "<neq>");
 //Gerais
-        lexemas.put("=", "atrib");
-        lexemas.put("int", "int,");
-        lexemas.put("float", "float,");
-        lexemas.put("str", "str,");
-        lexemas.put("var", "id,");
+        lexemas.put("=", "<atrib>");
+        lexemas.put("int", "<int>");
+        lexemas.put("float", "<float>");
+        lexemas.put("string", "<string>");
+        lexemas.put("var", "<id>");
 //Palavras-chave
 //Condicionais
-        lexemas.put("se", "cond");
-        lexemas.put("então", "initcond");
-        lexemas.put("senão", "altcond");
-        lexemas.put("fim-se", "endcond");
+        lexemas.put("se", "<cond>");
+        lexemas.put("então", "<initcond>");
+        lexemas.put("senão", "<altcond>");
+        lexemas.put("fim-se", "<endcond>");
 //Loops
-        lexemas.put("para", "forloop");
-        lexemas.put("de", "rng1forloop");
-        lexemas.put("até", "rng2forloop");
-        lexemas.put("faça", "initforloop");
-        lexemas.put("fim-para", "endforloop");
-        lexemas.put("enquanto", "whileloop");
-        lexemas.put("fim-enquanto", "endwhileloop");
+        lexemas.put("para", "<forloop>");
+        lexemas.put("de", "<rng1forloop>>");
+        lexemas.put("até", "<rng2forloop>>");
+        lexemas.put("faça", "<initforloop>>");
+        lexemas.put("fim-para", "<endforloop>>");
+        lexemas.put("enquanto", "<whileloop>>");
+        lexemas.put("fim-enquanto", "<endwhileloop>>");
 
     }
 
@@ -86,37 +81,141 @@ public class AnalisadorLexico {
     }
 
     public void serializa() throws IOException {
-//        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
-//                new FileOutputStream(path + ".lex"), "Cp1252"));
+        //Serializa o linkedhashmap de tokens e salva
         FileOutputStream fos
                 = new FileOutputStream(path + ".ser");
         ObjectOutputStream oos = new ObjectOutputStream(fos);
-        oos.writeObject(lexemas);
+        oos.writeObject(tokens);
         oos.close();
         fos.close();
-        //Serializa o linkedhashmap de tokens e salva
+
     }
 
-    public void deserializa() throws IOException, ClassNotFoundException {
-        FileInputStream fis = new FileInputStream(path + ".ser");
-        ObjectInputStream ois = new ObjectInputStream(fis);
-        HashMap<String, String> map = (HashMap) ois.readObject();
-        ois.close();
-        fis.close();
+    public void deserializa() throws ClassNotFoundException, IOException {
+        FileInputStream fis = null;
+        ObjectInputStream ois = null;
+        try {
+            fis = new FileInputStream(path + ".ser");
+            ois = new ObjectInputStream(fis);
+            tokens = (HashMap) ois.readObject();
+            ois.close();
+            fis.close();
+//        teste pra ver se serializacao funciona
 //        for (Map.Entry<String, String> entrySet : map.entrySet()) {
 //            String key = entrySet.getKey();
 //            String value = entrySet.getValue();
 //            System.out.println(key + " - " + value);
 //        }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(AnalisadorLexico.class.getName()).log(Level.SEVERE, "Arquivo não encontrado", ex);
+        }
     }
 
+    public BufferedReader carrega() throws FileNotFoundException, IOException {
+        FileInputStream file = new FileInputStream(path);
+        InputStreamReader isr = new InputStreamReader(file, "Cp1252");
+        BufferedReader br = new BufferedReader(isr);
+//        file.close();
+//        isr.close();
+        return br;
+    }
+
+    public void salva() throws FileNotFoundException, IOException {
+        File arquivo = new File(path + ".lex");
+        FileWriter fw = new FileWriter(arquivo, true);
+        for (Map.Entry<Integer, ArrayList<String>> entrySet : tokens.entrySet()) {
+            Integer key = entrySet.getKey();
+            ArrayList<String> value = entrySet.getValue();
+            fw.write("\n" + key + "\t");
+            for (String value1 : value) {
+                fw.write(value1);
+            }
+        }
+        fw.close();
+    }
+
+    public void salva(boolean substituir) throws FileNotFoundException, IOException {
+        File arquivo = new File(path + ".lex");
+        FileWriter fw = new FileWriter(arquivo, substituir);
+        for (Map.Entry<Integer, ArrayList<String>> entrySet : tokens.entrySet()) {
+            Integer key = entrySet.getKey();
+            ArrayList<String> value = entrySet.getValue();
+            fw.write("\n" + key + "\t");
+            for (String value1 : value) {
+                fw.write(value1);
+            }
+        }
+        fw.close();
+    }
+
+    public void startAnalysis() {
+        try {
+            BufferedReader br = carrega(path);
+            int count = 0;
+            while (br.ready()) {
+                count++;
+                tokens.put(count, analisa(br.readLine()));
+            }
+            br.close();
+        } catch (IOException ex) {
+            Logger.getLogger(AnalisadorLexico.class.getName()).log(Level.SEVERE, "Arquivo nao encontrado", ex);
+        }
+        for (Map.Entry<Integer, ArrayList<String>> entrySet : tokens.entrySet()) {
+            Integer key = entrySet.getKey();
+            ArrayList<String> value = entrySet.getValue();
+            System.out.print(key);
+            for (String value1 : value) {
+                System.out.print(" - " + value1);
+            }
+            System.out.println("");
+        }
+    }
+
+    private ArrayList<String> analisa(String s) {
+        ArrayList<String> listaTokens = new ArrayList<String>();
+        String token = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '#') {
+                return listaTokens;
+            }
+            if (s.charAt(i) == '"') {
+                i++;
+                token = lexemas.get("string")+"<";
+                while (s.charAt(i) != '"') {
+                    token += s.charAt(i);
+                    i++;
+                }
+                token+=">";
+                listaTokens.add(token);
+            } else if (ValidaLetra(s.charAt(i))) {
+                token = "";
+                token = token + s.charAt(i);
+                if (lexemas.containsKey(token) && !ValidaLetra(s.charAt(i + 1))) {
+                    token = lexemas.get(token) + "<" + token+">";
+                    listaTokens.add(token);
+                }
+//            else if(true){
+//                
+//            }
+            }
+        }
+        return listaTokens;
+    }
+
+    private boolean ValidaLetra(char letra) {
+        return (letra >= 65 && letra <= 90) || (letra >= 97 && letra <= 122);
+    }
+
+//    public boolean ValidaHifen(char simbolo, int indice) {
+//        return simbolo.charAt(indice) == '-' && ValidaLetra(simbolo.charAt(indice - 1)) && ValidaLetra(simbolo.charAt(indice + 1));
+//    }
     public static void main(String[] args) {
         try {
-            AnalisadorLexico al = new AnalisadorLexico("teste");
-            al.deserializa();
+            AnalisadorLexico al = new AnalisadorLexico("lprime.l");
+            al.startAnalysis();
+            al.salva(false);
+            //al.deserializa();
         } catch (IOException ex) {
-            Logger.getLogger(AnalisadorLexico.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ClassNotFoundException ex) {
             Logger.getLogger(AnalisadorLexico.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
--- Kalameet/src/Compiler/compilador.java
+++ Kalameet/src/Compiler/compilador.java
@@ -1,10 +1,31 @@
 package Compiler;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class compilador {
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        try {
+            //        try {
+            AnalisadorLexico al = new AnalisadorLexico("lprime.l");
+            al.startAnalysis();
+            al.salva(false);
+            //al.deserializa();
+//        } catch (IOException ex) {
+//            Logger.getLogger(AnalisadorLexico.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (ClassNotFoundException ex) {
+//            Logger.getLogger(AnalisadorLexico.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+        } catch (IOException ex) {
+            Logger.getLogger(AnalisadorLexico.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
 }
