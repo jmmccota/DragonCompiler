4c20d3a4a0ee4c9f384922a29014c3abffd84275
jmmccota <joao_marcos_cota@hotmail.com>
2015-03-20 16:18:09 -0300

    analise para igualadores

--- Kalameet/src/Compiler/AnalisadorLexico.java
+++ Kalameet/src/Compiler/AnalisadorLexico.java
@@ -16,18 +16,19 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import jdk.nashorn.internal.codegen.types.NumericType;
 
 /**
  * @author JM
  */
 public class AnalisadorLexico {
 
-    private Map<Integer, ArrayList<String>> tokens;
+    private Map<Integer, ArrayList<Token>> tokens;
     private Map<String, String> lexemas;
     private String path;
 
     public AnalisadorLexico(String path) {
-        tokens = new LinkedHashMap<Integer, ArrayList<String>>();
+        tokens = new LinkedHashMap<Integer, ArrayList<Token>>();
         lexemas = new HashMap<String, String>();
         this.path = path;
         preencheLexemas();
@@ -35,41 +36,41 @@ public class AnalisadorLexico {
 
     private void preencheLexemas() {
 //Aritmeticos
-        lexemas.put("+", "<sum>");
-        lexemas.put("-", "<sub>");
-        lexemas.put("*", "<mult>");
-        lexemas.put("x", "<mult>");
-        lexemas.put("/", "<div>");
-        lexemas.put(":", "<div>");
-        lexemas.put(".", "<.>");
-        lexemas.put(",", "<.>");
+        lexemas.put("+", "sum");
+        lexemas.put("-", "sub");
+        lexemas.put("*", "mult");
+        lexemas.put("x", "mult");
+        lexemas.put("/", "div");
+        lexemas.put(":", "div");
+        lexemas.put(".", ".");
+        lexemas.put(",", ".");
 //Comparativos
-        lexemas.put(">", "<gt>");
-        lexemas.put(">=", "<gte>");
-        lexemas.put("<", "<lt>");
-        lexemas.put("=<", "<lte>");
-        lexemas.put("==", "<eq>");
-        lexemas.put("!=", "<neq>");
+        lexemas.put(">", "gt");
+        lexemas.put(">=", "gte");
+        lexemas.put("<", "lt");
+        lexemas.put("=<", "lte");
+        lexemas.put("==", "eq");
+        lexemas.put("!=", "neq");
 //Gerais
-        lexemas.put("=", "<atrib>");
-        lexemas.put("int", "<int>");
-        lexemas.put("float", "<float>");
-        lexemas.put("string", "<string>");
-        lexemas.put("var", "<id>");
+        lexemas.put("=", "atrib");
+        lexemas.put("int", "int");
+        lexemas.put("float", "float");
+        lexemas.put("string", "string");
+        lexemas.put("var", "id");
 //Palavras-chave
 //Condicionais
-        lexemas.put("se", "<cond>");
-        lexemas.put("então", "<initcond>");
-        lexemas.put("senão", "<altcond>");
-        lexemas.put("fim-se", "<endcond>");
+        lexemas.put("se", "cond");
+        lexemas.put("então", "initcond");
+        lexemas.put("senão", "altcond");
+        lexemas.put("fim-se", "endcond");
 //Loops
-        lexemas.put("para", "<forloop>");
-        lexemas.put("de", "<rng1forloop>>");
-        lexemas.put("até", "<rng2forloop>>");
-        lexemas.put("faça", "<initforloop>>");
-        lexemas.put("fim-para", "<endforloop>>");
-        lexemas.put("enquanto", "<whileloop>>");
-        lexemas.put("fim-enquanto", "<endwhileloop>>");
+        lexemas.put("para", "forloop");
+        lexemas.put("de", "rng1forloop");
+        lexemas.put("até", "rng2forloop");
+        lexemas.put("faça", "initforloop");
+        lexemas.put("fim-para", "endforloop");
+        lexemas.put("enquanto", "whileloop");
+        lexemas.put("fim-enquanto", "endwhileloop");
 
     }
 
@@ -123,12 +124,12 @@ public class AnalisadorLexico {
     public void salva() throws FileNotFoundException, IOException {
         File arquivo = new File(path + ".lex");
         FileWriter fw = new FileWriter(arquivo, true);
-        for (Map.Entry<Integer, ArrayList<String>> entrySet : tokens.entrySet()) {
+        for (Map.Entry<Integer, ArrayList<Token>> entrySet : tokens.entrySet()) {
             Integer key = entrySet.getKey();
-            ArrayList<String> value = entrySet.getValue();
-            fw.write("\n" + key + "\t");
-            for (String value1 : value) {
-                fw.write(value1);
+            ArrayList<Token> value = entrySet.getValue();
+            //fw.write("\n" + key + "\t");
+            for (Token value1 : value) {
+                fw.write(value1.getValor() + "\n");
             }
         }
         fw.close();
@@ -137,12 +138,12 @@ public class AnalisadorLexico {
     public void salva(boolean substituir) throws FileNotFoundException, IOException {
         File arquivo = new File(path + ".lex");
         FileWriter fw = new FileWriter(arquivo, substituir);
-        for (Map.Entry<Integer, ArrayList<String>> entrySet : tokens.entrySet()) {
+        for (Map.Entry<Integer, ArrayList<Token>> entrySet : tokens.entrySet()) {
             Integer key = entrySet.getKey();
-            ArrayList<String> value = entrySet.getValue();
-            fw.write("\n" + key + "\t");
-            for (String value1 : value) {
-                fw.write(value1);
+            ArrayList<Token> value = entrySet.getValue();
+            //fw.write("\n" + key + "\t");
+            for (Token value1 : value) {
+                fw.write(value1.getValor() + "\n");
             }
         }
         fw.close();
@@ -152,71 +153,96 @@ public class AnalisadorLexico {
         try {
             BufferedReader br = carrega(path);
             int count = 0;
+            String s = "";
+            boolean comentario = false;
+            ArrayList<Token> listaTokens;
             while (br.ready()) {
+                listaTokens = new ArrayList<Token>();
                 count++;
-                tokens.put(count, analisa(br.readLine()));
+                s = br.readLine();
+                Token token = null;
+                String t;// = "";
+                for (int i = 0; i < s.length(); i++) {
+                    if (s.charAt(i) == '#') {
+                        comentario = !comentario;
+                    }
+
+                    if (!comentario) {
+
+                        if (s.charAt(i) == '"') {
+                            t = "";
+                            i++;
+                            while (s.charAt(i) != '"') {
+                                t += s.charAt(i);
+                                i++;
+                            }
+                            token = new Token(lexemas.get("string"), t);
+                            listaTokens.add(token);
+                        } else if (Character.isLetter(s.charAt(i))) {
+                            t = "";
+                            t += s.charAt(i);
+                            if (lexemas.containsKey(t)) {// && !ValidaLetra(s.charAt(i + 1))) {
+                                //t = "" + t + "";
+                                token = new Token(lexemas.get(t), t);
+                                listaTokens.add(token);
+                            }
+                        } else if (s.charAt(i) == '>' || s.charAt(i) == '<' || s.charAt(i) == '=') {
+                            t = "";
+                            t += s.charAt(i);
+                            if (s.length() > (i+1) && (s.charAt(i + 1) == '>' || s.charAt(i + 1) == '<' || s.charAt(i + 1) == '=')) {
+                                String proximo = t + s.charAt(i + 1);
+                                if (lexemas.containsKey(proximo)) {
+                                    t = proximo;
+                                }
+                                i++;
+                            }                             
+                            listaTokens.add(new Token(lexemas.get(t), t));
+                        } else if((i+1)<s.length() && (s.charAt(i)+""+s.charAt(i+1)).equals("!=")){
+                            t = s.charAt(i)+""+s.charAt(i+1);
+                            listaTokens.add(new Token(lexemas.get(t), t));
+                            i++;
+                        }
+                        if (Character.isDigit(s.charAt(i))) {
+
+                        }
+//                        if (lexemas.containsKey(s.charAt(i)+"")) {// && !ValidaLetra(s.charAt(i + 1))) {
+//                            t = "";
+//                            t = "" + t + "";
+//                            token = new Token(lexemas.get(t), t);
+//                            listaTokens.add(token);
+//                        }
+                    }
+                }
+
+                tokens.put(count, listaTokens);
             }
             br.close();
         } catch (IOException ex) {
             Logger.getLogger(AnalisadorLexico.class.getName()).log(Level.SEVERE, "Arquivo nao encontrado", ex);
         }
-        for (Map.Entry<Integer, ArrayList<String>> entrySet : tokens.entrySet()) {
+        for (Map.Entry<Integer, ArrayList<Token>> entrySet : tokens.entrySet()) {
             Integer key = entrySet.getKey();
-            ArrayList<String> value = entrySet.getValue();
-            System.out.print(key);
-            for (String value1 : value) {
-                System.out.print(" - " + value1);
+            ArrayList<Token> value = entrySet.getValue();
+            System.out.print(key+" - ");
+            for (Token value1 : value) {
+                System.out.print(value1.toString()+" ");
             }
             System.out.println("");
         }
-    }
-
-    private ArrayList<String> analisa(String s) {
-        ArrayList<String> listaTokens = new ArrayList<String>();
-        String token = "";
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '#') {
-                return listaTokens;
-            }
-            if (s.charAt(i) == '"') {
-                i++;
-                token = lexemas.get("string")+"<";
-                while (s.charAt(i) != '"') {
-                    token += s.charAt(i);
-                    i++;
-                }
-                token+=">";
-                listaTokens.add(token);
-            } else if (ValidaLetra(s.charAt(i))) {
-                token = "";
-                token = token + s.charAt(i);
-                if (lexemas.containsKey(token) && !ValidaLetra(s.charAt(i + 1))) {
-                    token = lexemas.get(token) + "<" + token+">";
-                    listaTokens.add(token);
-                }
-//            else if(true){
-//                
-//            }
-            }
-        }
-        return listaTokens;
-    }
-
-    private boolean ValidaLetra(char letra) {
-        return (letra >= 65 && letra <= 90) || (letra >= 97 && letra <= 122);
     }
 
 //    public boolean ValidaHifen(char simbolo, int indice) {
 //        return simbolo.charAt(indice) == '-' && ValidaLetra(simbolo.charAt(indice - 1)) && ValidaLetra(simbolo.charAt(indice + 1));
 //    }
     public static void main(String[] args) {
-        try {
-            AnalisadorLexico al = new AnalisadorLexico("lprime.l");
-            al.startAnalysis();
-            al.salva(false);
-            //al.deserializa();
-        } catch (IOException ex) {
-            Logger.getLogger(AnalisadorLexico.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        System.out.println(Character.isAlphabetic('>'));
+//        try {
+//            AnalisadorLexico al = new AnalisadorLexico("lprime.l");
+//            al.startAnalysis();
+//            al.salva(false);
+//            //al.deserializa();
+//        } catch (IOException ex) {
+//            Logger.getLogger(AnalisadorLexico.class.getName()).log(Level.SEVERE, null, ex);
+//        }
     }
 }
---
+++ Kalameet/src/Compiler/Token.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Compiler;
+
+/**
+ *
+ * @author JM
+ */
+public class Token {
+
+    private String tipo;
+    private String valor;
+
+    public Token(String tipo, String valor) {
+        this.tipo = tipo;
+        this.valor = valor;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return "<" + tipo + "," + valor + ">";
+    }
+
+}
