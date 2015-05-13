2974e50089d1de8b690d488b5a8a805af7573ed0
jmmccota <joao_marcos_cota@hotmail.com>
2015-05-13 10:46:38 -0300

    adicionada matriz dinamica... arvore em branco ainda

--- Kalameet/src/Compiler/Arvore.java
+++ Kalameet/src/Compiler/Arvore.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Compiler;
 
 /**
  *
  * @author JM
+ * @param <S>
  */
-public class Arvore {
+public class Arvore<S> {
     
 }
---
+++ Kalameet/src/Compiler/MatrizDinamica.java
@@ -0,0 +1,34 @@
+package Compiler;
+import java.util.HashMap;
+import java.util.Map;
+public class MatrizDinamica<T> {
+
+    private final Map<Integer, Map<Integer, T>> elementos = new HashMap<Integer, Map<Integer, T>>();
+
+    public void set(int linha, int coluna, T elemento) {
+        Map<Integer, T> colunas = getColunas(linha);
+        Integer chave = Integer.valueOf(coluna);
+        if (elemento != null) {
+            colunas.put(chave, elemento);
+        } else {
+            colunas.remove(chave);
+        }
+    }
+
+    public T get(int linha, int coluna) {
+        Map<Integer, T> colunas = getColunas(linha);
+        Integer chave = Integer.valueOf(coluna);
+        T elemento = colunas.get(chave);
+        return elemento;
+    }
+
+    private Map<Integer, T> getColunas(int linha) {
+        int chave = Integer.valueOf(linha);
+        Map<Integer, T> colunas = elementos.get(chave);
+        if (colunas == null) {
+            colunas = new HashMap<Integer, T>();
+            elementos.put(chave, colunas);
+        }
+        return colunas;
+    }
+}
